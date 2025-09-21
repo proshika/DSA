e600d9be87553130ad3ae5eee1e89a1c58f4065f
Proshika Patle <patleproshika06@gmail.com>
2025-09-21 16:31:43 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (94.90%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        int[] parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) parent[i] = i;
+
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            int pu = find(parent, u), pv = find(parent, v);
+            if (pu == pv) return edge; // Found redundant connection
+            parent[pu] = pv; // Union
+        }
+        return new int[0];
+    }
+
+    private int find(int[] parent, int x) {
+        if (parent[x] != x) parent[x] = find(parent, parent[x]);
+        return parent[x];
+    }
+}
