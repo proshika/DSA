82fadf00a61348797e9a1060b4589f3ccb686aba
Proshika Patle <patleproshika06@gmail.com>
2025-09-25 08:19:34 +0530

    Time: 11 ms (38.19%) | Memory: 57.5 MB (11.18%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private ListNode merge(ListNode head1,ListNode head2){
+        ListNode mergeLL=new ListNode(-1);
+        ListNode temp=mergeLL;
+        while(head1!=null && head2!=null){
+            if(head1.val<=head2.val){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }
+            else{
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        }
+        while(head1!=null){
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+        while(head2!=null){
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+        }
+        return mergeLL.next;
+    }
+    private ListNode getmid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode mid=getmid(head);
+        ListNode righthead=mid.next;
+        mid.next=null;
+        ListNode newleft=sortList(head);
+        ListNode newright=sortList(righthead);
+        return merge(newleft,newright);
+    }
+}
