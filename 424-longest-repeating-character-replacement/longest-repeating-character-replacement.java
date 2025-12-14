74770bd51020d41960445abb6c66402ebdf7319b
Proshika Patle <patleproshika06@gmail.com>
2025-12-14 12:23:21 +0530

    Time: 7 ms (87.45%) | Memory: 46 MB (55.23%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int left=0,maxfreq=0,maxlen=0;
+        int[] count=new int[26];
+
+        for(int right=0;right<s.length();right++){
+            count[s.charAt(right)-'A']++;
+            maxfreq=Math.max(maxfreq,count[s.charAt(right)-'A']);
+
+            while(right-left+1-maxfreq>k){
+                count[s.charAt(left++)-'A']--;
+            }
+            maxlen=Math.max(maxlen,right-left+1);
+        }
+        return maxlen;
+    }
+}
