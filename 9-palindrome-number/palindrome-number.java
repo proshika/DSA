875821dda52bc484bcc546c0b82637c290c86f1d
Proshika Patle <patleproshika06@gmail.com>
2025-12-19 09:36:18 +0530

    Time: 5 ms (73.90%) | Memory: 46.1 MB (20.52%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int div=1;
+        while(x/div>=10){
+            div*=10;
+        }
+        while(x>0){
+            int left=x/div,right=x%10;
+            if(left!=right){
+                return false;
+            }
+            x=(x%div)/10;
+            div=div/100;
+        } 
+        return true;
+    }
+}
