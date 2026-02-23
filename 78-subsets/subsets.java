dc7005c6851a41adea0b823fb6a8398af9a3397e
Proshika Patle <patleproshika06@gmail.com>
2026-02-23 11:08:27 +0530

    Time: 1 ms (87.91%) | Memory: 44.4 MB (37.99%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
+        helper(nums,0,new ArrayList<>(),result);
+        return result;
+    }
+    private void helper(int nums[],int start,List<Integer> path, List<List<Integer>> result){
+        result.add(new ArrayList<>(path));
+        for(int i=start;i<nums.length;i++){
+            path.add(nums[i]);
+            helper(nums,i+1,path,result);
+            path.remove(path.size()-1);
+        }
+    }
+}
