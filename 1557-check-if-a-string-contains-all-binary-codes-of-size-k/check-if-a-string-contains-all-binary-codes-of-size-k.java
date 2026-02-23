2182561537f84da122089a1b753e751902bdc12b
Proshika Patle <patleproshika06@gmail.com>
2026-02-23 23:39:26 +0530

    Time: 153 ms (56.79%) | Memory: 83.4 MB (74.57%) - LeetSync

---
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        int stringLength = s.length();
+        // Calculate total number of possible k-bit binary codes (2^k)
+        int totalPossibleCodes = 1 << k;
+      
+        // Early termination: if string doesn't have enough substrings, return false
+        // Number of k-length substrings = stringLength - k + 1
+        if (stringLength - k + 1 < totalPossibleCodes) {
+            return false;
+        }
+      
+        // Use HashSet to store unique k-length substrings
+        Set<String> uniqueSubstrings = new HashSet<>();
+      
+        // Iterate through all possible k-length substrings
+        for (int i = 0; i <= stringLength - k; i++) {
+            // Extract substring from index i to i+k (exclusive)
+            String currentSubstring = s.substring(i, i + k);
+            uniqueSubstrings.add(currentSubstring);
+        }
+      
+        // Check if we found all possible k-bit binary codes
+        return uniqueSubstrings.size() == totalPossibleCodes;
+
+    }
+}
