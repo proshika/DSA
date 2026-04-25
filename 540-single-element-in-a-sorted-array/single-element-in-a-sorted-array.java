c3ca63a64ecb64ad246f6fa4b5b7dba0a1b03f3a
Proshika Patle <patleproshika06@gmail.com>
2026-04-25 12:43:53 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (59.12%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        Map<Integer, Integer> hm = new HashMap<>();
+        /*Map<Integer, Integer> hm = new HashMap<>();
         int ans = 0;
         for (int num : nums) {
             hm.put(num, hm.getOrDefault(num, 0) + 1);
@@ -11,5 +11,18 @@ class Solution {
             }
         }
         return ans;
+        */
+        int l=0,r=nums.length-1;
+            while(l<r){
+                int mid=l+(r-l)/2;
+                if(mid%2==1) mid--;
+                if(nums[mid]==nums[mid+1]){
+                    l=mid+2;
+                }
+                else{
+                    r=mid;
+                }
+            }            
+        return nums[l];
     }
 }
