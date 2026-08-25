89629fcbe67b474c6675eb7478cca39df6bb43d7
Proshika Patle <patleproshika06@gmail.com>
2026-08-25 14:48:04 +0530

    Time: 2 ms (68.20%) | Memory: 45.2 MB (61.63%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        for(int i=k; ;i=i+k){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+    }
+}
